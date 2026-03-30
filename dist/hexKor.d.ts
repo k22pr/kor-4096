e6164935d5511f004caa226727266a3a6cca17da
ksPr <h2x@pm.me>
2026-03-31 02:46:36 +0900

    feat : Bun 빌드 시스템 적용

--- dist/hexKor.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function encode(input: string | Buffer | number): string;
-export declare function decode(kor: string): string;
-export declare function decodeToBuffer(kor: string): Buffer;
-export declare function encodeArray(list: (string | Buffer | number)[]): string[];
-export declare function decodeArray(list: string[]): string[];
